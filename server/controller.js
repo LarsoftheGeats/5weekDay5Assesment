e22d9a18856ea0f1944fb6ceef5bf9fb6cf4852f
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-10-28 11:08:40 -0600

    add cities workds

--- server/controller.js
+++ server/controller.js
@@ -1,19 +1,36 @@
 require('dotenv').config()
 const {CONNECTION_STRING} = process.env
-const Sequelize = require('sequelize')
 
-const sequelize= new Sequelize(CONNECTION_STRING, {
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(CONNECTION_STRING, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false
         }
     }
-  })
+})
 
 module.exports = {
+    getCountries: (req,res) => {
+        sequelize.query(`select * from countries`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    createCity: (req,res) => {
+        const {name,rating,countryId} = req.body
+        sequelize.query(`
+            Insert into cities (name, rating, country_id)
+            values ('${name}', ${rating}, ${countryId});
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+
+    },
+
     seed: (req, res) => {
-        //console.log(req)
         sequelize.query(`
             drop table if exists cities;
             drop table if exists countries;
@@ -26,8 +43,8 @@ module.exports = {
             CREATE TABLE cities(
                 city_id SERIAL PRIMARY KEY,
                 name VARCHAR,
-                rating integer,
-                country_id integer references countries(country_id)
+                rating INTEGER,
+                country_id integer REFERENCES countries(country_id)
             );
 
             insert into countries (name)
